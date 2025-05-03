51926660c50be49054baacea4f60a30578e8cb6d
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-05-03 09:21:25 +0530

    feat: Create authentication routes, controllers and middleware

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,15 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import AuthRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
 app.get("/", (req, res) => {
-  res.status(200).send(
-    "A fun, student-focused platform to master Data Structures and Algorithms with interactive challenges and clear explanations. Built with React and Node.js for a seamless learning experience."
-  );
+  res
+    .status(200)
+    .send(
+      "A fun, student-focused platform to master Data Structures and Algorithms with interactive challenges and clear explanations. Built with React and Node.js for a seamless learning experience."
+    );
 });
+
+app.use("/api/v1/auth", AuthRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on 8080");
---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  check,
+  login,
+  logout,
+  register,
+} from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+
+const AuthRoutes = express.Router();
+
+AuthRoutes.post("/register", register);
+
+AuthRoutes.post("/login", login);
+
+AuthRoutes.post("/logout",authMiddleware, logout);
+
+AuthRoutes.post("/check",authMiddleware, check);
+
+export default AuthRoutes;
