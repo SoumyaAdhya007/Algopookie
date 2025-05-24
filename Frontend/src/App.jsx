504d9f9f00a4833632fd584fdbd6ae08e1f4a3f2
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-05-24 19:36:17 +0530

    feat: add problem page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Layout from "./components/Layout";
 import { useAuthStore } from "./store/useAuthStore";
 import AdminRoute from "./components/AdminRoute";
 import AddProblem from "./page/AddProblem";
+import ProblemPage from "./page/ProblemPage";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -45,6 +46,12 @@ const App = () => {
           path="/signup"
           element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
         />
+
+        <Route
+          path="/problem/:id"
+          element={authUser ? <ProblemPage /> : <Navigate to={"/login"} />}
+        />
+
         <Route element={<AdminRoute />}>
           <Route
             path="/add-problem"
