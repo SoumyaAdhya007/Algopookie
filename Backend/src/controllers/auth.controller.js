98ade25946260967dadfab74e126cb5aa25786ea
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-06-07 17:30:41 +0530

    fix: added frontend URL to CORS configuration

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,7 +42,7 @@ export const register = async (req, res) => {
 
     res.cookie("jwt", token, {
       httpOnly: true,
-      samesite: "none",
+      sameSite: "none",
       secure: process.env.NODE_ENV !== "development",
       maxAge: 1000 * 60 * 60 * 24 * 7, //7 days
     });
@@ -101,7 +101,7 @@ export const login = async (req, res) => {
 
     res.cookie("jwt", token, {
       httpOnly: true,
-      samesite: "none",
+      sameSite: "none",
       secure: process.env.NODE_ENV !== "development",
       maxAge: 1000 * 60 * 60 * 24 * 7, //7 days
     });
@@ -134,7 +134,7 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("jwt", {
       httpOnly: true,
-      samesite: "none",
+      sameSite: "none",
       secure: process.env.NODE_ENV !== "development",
     });
     res.status(200).json({
